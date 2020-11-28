ff834e916137b9039a56faff354f545864718228
hyejeong14 <jiu1470@icloud.com>
2020-11-29 08:16:16 +0900

    fever file

--- Fever.py
+++ Fever.py
@@ -10,12 +10,3 @@ class Fever():
 
     def feverTime(self,hero_sprites,ghost_sprites):
         pygame.sprite.groupcollide(hero_sprites, ghost_sprites, False, True)
-
-'''def feverTime():
-    global BALLS
-    for i in range(2):
-        BALLS.append(Block((200, 242, 0), Rect(300, 400, 20, 20), 10))
-    for BALL in BALLS:
-        BALL.speed = 10
-
-        pygame.sprite.groupcollide(hero_sprites, ghost_sprites, False, True):'''
