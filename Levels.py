bec4891a3158e44c5e6b90a39119dd9c97905fcf
voiagerd <voiagerd@dgu.edu>
2020-12-13 15:19:59 +0900

    dec 13

---
+++ Levels.py
@@ -0,0 +1,248 @@
+import pygame
+from Sprites import *
+import random
+import Game14
+
+MODE= 'randomMode'
+
+
+'''
+setupWalls 원리
+- width/70 : 벽의 두께 (+- width/70 : 위치 조절하려고 두께만큼 더하거나 뺌)
+- 원래 맵 사진에서 벽 위치의 비율에 따라 적절한 수를 곱함
+- [x, y, width, height]
+
+setupGate, setupFood
+- 화면에 표시되는 Gate와 Food의 비율을 계산하여 적절한 비율을 곱함
+'''
+
+# 좌표개 x축이랑 거리 y축이랑 거리
+
+class Level1():
+	MODE='EASY'
+	def __init__(self):
+		self.info = 'level1'
+	'''벽 만들기'''
+	def setupWalls(self, wall_color,width,height):
+		self.wall_sprites = pygame.sprite.Group()
+		wall_positions = [[0, 0, width/70, height], # 맨 왼쪽
+	                    [0, 0, width, width/70], # 맨 위쪽
+	                    [0, height-width/70, width, width/70], # 맨 아래쪽
+	                    [width-width/70, 0, width/6, height], # 맨 오른쪽
+	                    [width/4+width/70, 0, width/70, height/5+width/70], # 맨 위 왼쪽 |
+	                    [width*3/4, 0, width/70, height/5+width/70], # 맨 위 오른쪽 |체크
+	                    [width/3 + width/20, height/5, width/4, width/70], # ghost zone 위 가운데 -----
+	                    [width*2/19, height/5, width/18+width/70, width/70], [width*2/19, height/5, width/70, height*2/9], # 맨 위 왼쪽 ㄱ자 좌우반전한것
+	                    [width*16/19, height/5, width/18+width/70, width/70], [width*17/19+4, height/5, width/70, height*2/9], # 맨 위 오른쪽 ㄱ자 좌우반전한것
+	                    [width*4/19, height*2/5+width/70, width/18+width/70, width/70], [width*3/4, height*2/5+width/70, width/18+width/70, width/70], [width*4/19, height*3/5+5, width/18+width/70, width/70], [width*3/4, height*3/5+5, width/18+width/70, width/70], # 조그마한 - 4개
+	                    [width*2/19, height*4/5+5, width/18+width/70, width/70], [width*2/19, height*3/5, width/70, height*2/9], # 아래 왼쪽 L자
+	                    [width*16/19, height*4/5+5, width/18+width/70, width/70], [width*17/19+4, height*3/5, width/70, height*2/9], # 아래 오른쪽 L자 좌우반전한것
+	                    [width/4+width/70, height*4/5-width/70 , width/70, height/5+width/70], # 맨 아래 왼쪽 |
+	                    [width*3/4, height*4/5-width/70, width/70, height/5+width/70], # 맨 아래 오른쪽 |
+	                    [width/3 + width/20, height*4/5+width/70, width/4, width/70], # ghost zone 아래 가운데 -----
+	                    [width/3 + width/20, height*2/5, width/18+width/70, width/70], # ghost zone start 가로
+	                    [width/3 + width/20, height*2/5, width/70, height*2/9], #세로
+	                    [width/3 + width/20, height*3/5+5, width/4, width/70], #가로
+	                    [width*11/19-5, height*2/5, width/18+width/70, width/70], #가로
+	                    [width*12/19, height*2/5+5, width/70, height/5+width/70], # ghost zone fin 세로
+	                    ]
+		for wall_position in wall_positions:
+			wall = Wall(*wall_position, wall_color)
+			self.wall_sprites.add(wall)
+		return self.wall_sprites
+
+	# EASY version ghost zone 문 만들기
+	def setupGate(self, gate_color, width, height):
+		self.gate_sprites = pygame.sprite.Group()
+		self.gate_sprites.add(Wall(width/3 + width*2/18, height*2/5, width*2/18+width/70, 2, gate_color))
+		return self.gate_sprites
+
+	# EASY version 캐릭터 구축
+	def setupPlayers(self, hero_image_path, ghost_images_path,width,height):
+		self.hero_sprites = pygame.sprite.Group()
+		self.ghost_sprites = pygame.sprite.Group()
+		self.hero_sprites.add(Player(width/2, height*6/9, hero_image_path)) #439
+		for each in ghost_images_path:
+			# 유령 이동경로 setup
+			role_name = each.split('/')[-1].split('.')[0]
+			if role_name == 'Blinky': # 빨간색
+				player = Player(257, 140, each)
+				player.is_move = True
+				player.tracks = [[0, -0.5, 4], [0.5, 0, 9], [0, 0.5, 11], [0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 11], [0, 0.5, 3],
+								 [0.5, 0, 15], [0, -0.5, 15], [0.5, 0, 3], [0, -0.5, 11], [-0.5, 0, 3], [0, -0.5, 11], [-0.5, 0, 3],
+								 [0, -0.5, 3], [-0.5, 0, 7], [0, -0.5, 3], [0.5, 0, 15], [0, 0.5, 15], [-0.5, 0, 3], [0, 0.5, 3],
+								 [-0.5, 0, 3], [0, -0.5, 7], [-0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 11], [0, -0.5, 7], [0.5, 0, 5]]
+
+				self.ghost_sprites.add(player)
+			elif role_name == 'Clyde':
+				player = Player(289, 140, each)
+				player.is_move = True
+				player.tracks = [[-1, 0, 2], [0, -0.5, 4], [0.5, 0, 5], [0, 0.5, 7], [-0.5, 0, 11], [0, -0.5, 7],
+								 [-0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 7], [0, 0.5, 15], [0.5, 0, 15], [0, -0.5, 3],
+								 [-0.5, 0, 11], [0, -0.5, 7], [0.5, 0, 3], [0, -0.5, 11], [0.5, 0, 9]]
+				self.ghost_sprites.add(player)
+			elif role_name == 'Inky':
+				player = Player(225, 140, each)
+				player.is_move = True
+				player.tracks = [[1, 0, 2], [0, -0.5, 4], [0.5, 0, 10], [0, 0.5, 7], [0.5, 0, 3], [0, -0.5, 3],
+								 [0.5, 0, 3], [0, -0.5, 15], [-0.5, 0, 15], [0, 0.5, 3], [0.5, 0, 15], [0, 0.5, 11],
+								 [-0.5, 0, 3], [0, -0.5, 7], [-0.5, 0, 11], [0, 0.5, 3], [-0.5, 0, 11], [0, 0.5, 7],
+								 [-0.5, 0, 3], [0, -0.5, 3], [-0.5, 0, 3], [0, -0.5, 15], [0.5, 0, 15], [0, 0.5, 3],
+								 [-0.5, 0, 15], [0, 0.5, 11], [0.5, 0, 3], [0, -0.5, 11], [0.5, 0, 11], [0, 0.5, 3], [0.5, 0, 1]]
+				self.ghost_sprites.add(player)
+			elif role_name == 'Pinky':
+				player = Player(319, 140, each)
+				player.is_move = True
+				player.tracks = [[0, -1, 4], [0.5, 0, 9], [0, 0.5, 11], [-0.5, 0, 23], [0, 0.5, 7], [0.5, 0, 3],
+								 [0, -0.5, 3], [0.5, 0, 19], [0, 0.5, 3], [0.5, 0, 3], [0, 0.5, 3], [0.5, 0, 3],
+								 [0, -0.5, 15], [-0.5, 0, 7], [0, 0.5, 3], [-0.5, 0, 19], [0, -0.5, 11], [0.5, 0, 9]]
+				self.ghost_sprites.add(player)
+		return self.hero_sprites, self.ghost_sprites
+
+
+	# EASY version setupFood
+	def setupFood(self, food_color, bg_color, width, height):
+		self.food_sprites = pygame.sprite.Group()
+		# EASY : 가로 18개, 세로 9개 아이템 생성
+		# --> ROW = 9, COL = 18
+		for row in range(9):
+			for col in range(18):
+				if (row == 3 or row == 4) and (col == 7 or col == 8 or col == 9 or col == 10):
+					continue
+				else:
+					if ((row == 1 or row == 8) and (col == 0 or col == 18)) or ((row == 2 or row == 5) and (col == 0 or col == 3 or col == 15)):
+						food = Food(width/19*col+width/19, height/10*row+height/10, width/30, width/30, food_color, bg_color)
+					else:
+						food = Food(width/19*col+width/19, height/10*row+height/10, width/50, width/50, food_color, bg_color)
+				is_collide = pygame.sprite.spritecollide(food, self.wall_sprites, False)
+				if is_collide:
+					continue
+				is_collide = pygame.sprite.spritecollide(food, self.hero_sprites, False)
+				if is_collide:
+					continue
+				self.food_sprites.add(food)
+		return self.food_sprites
+
+class Level2():
+	MODE = 'HARD'
+	def __init__(self):
+		self.info = 'level2'
+	'''벽 만들기'''
+	def setupWalls(self, wall_color,width,height):
+		self.wall_sprites = pygame.sprite.Group()
+		wall_positions = [[0, 0, width/70, height], # 외벽
+						  [0, 0, width, width/70],
+						  [0, height-width/70, width, width/70],
+						  [width-width/70, 0, width/70, height],
+						  [width*7/20, height*8/19-width/70, width*2/19, width/70], # ghost zone start 가로
+						  [width*7/20, height*8/19-width/70, width/70, height*2/19+width/70], #세로
+						  [width*7/20, height*10/19-width/70, width*6/19-width/70, width/70], # 가로
+						  [width*11/20, height*8/19-width/70, width*2/19, width/70], #가로
+						  [width*13/20, height*8/19-width/70, width/70, height*2/19+width/70], # ghost zone fin 세로
+						  [width*8/20, 0, width/70, height*2/19+width/70], # 맨 위 왼쪽 |
+						  [width*12/20, 0, width/70, height*2/19+width/70], # 맨 위 오른쪽 |
+						  [width*2/20, height*2/19, width*4/19, width/70], [width*2/20, height*2/19, width/70, height/19+width/70], # 맨 위 왼쪽 ㄱ자 좌우반전한것
+						  [width*14/20, height*2/19, width*4/19, width/70], [width*18/20, height*2/19, width/70, height/19+width/70], # 맨 위 오른쪽 ㄱ자 좌우반전한것
+						  [width*10/20, height*2/19, width/70, height*2/19+width/70], # 맨 위 두번째 가운데 |
+						  [width*6/20, height*4/19, width*2/19, width/70], # 맨 위 두번째 왼쪽  -
+						  [width*12/20, height*4/19, width*2/19, width/70],  # 맨 위 두번째 오른쪽  -
+						  [0, height*5/19-width/70, width*2/19, width/70], # 맨 왼쪽 외벽쪽 -
+						  [width*18/20, height*5/19-width/70, width*2/19-width/70, width/70], # 맨 오른쪽 외벽쪽 -
+						  [width*4/20, height*4/19-width/70, width/70, height*2/19+width/70], [width*4/20, height*6/19-width/70, width/19+width/70, width/70], # 맨 위 왼쪽 L자
+						  [width*16/20, height*4/19-width/70, width/70, height*2/19+width/70], [width*15/20, height*6/19-width/70, width/19, width/70], # 맨 위 오른쪽 L자 좌우반전
+						  [width*7/20, height*6/19-width/70, width*6/19, width/70], # ghost zone 위 가운데 -----
+						  [width*2/20, height*7/19-width/70, width/70, height/19+width/70], [width*2/20, height*8/19-width/70, width/19+width/70, width/70], # ghost zone 왼쪽 조그만 ㄴ자
+						  [width*18/20, height*7/19-width/70, width/70, height/19+width/70], [width*17/20, height*8/19-width/70, width/19, width/70], # ghost zone 오른쪽 조그만 ㄴ자
+						  [width*5/20, height*8/19-width/70, width/70, height*2/19+width/70], [width*4/20, height*10/19-width/70, height/19, width/70], # ghost zone 바로 왼쪽 L자 좌우반전
+						  [width*15/20, height*8/19-width/70, width/70, height*2/19+width/70], [width*15/20, height*10/19-width/70, height/19+width/70, width/70], # ghost zone 바로 오른쪽 L자 좌우반전
+						  [width*2/20, height*10/19-width/70, width/70, height*2/19], [width*2/20, height*12/19-width*2/70, width/19+width/70, width/70], # L자
+						  [width*18/20, height*10/19-width/70, width/70, height*2/19], [width*17/20, height*12/19-width*2/70, width/19, width/70], # L자 좌우반전
+						  [width*5/20, height*12/19-width*2/70, width/70, height*2/19+width/70], [width*5/20, height*12/19-width*2/70, width*3/19, width/70], # ㅗ자 감싸는 wall 왼쪽
+						  [width*15/20, height*12/19-width*2/70, width/70, height*2/19+width/70], [width*12/20, height*12/19-width*2/70, width*3/19, width/70], # ㅗ자 감싸는 wall 오른쪽
+						  [width*10/20, height*12/19-width*2/70, width/70, height*2/19+width/70], [width*7/20, height*14/19-width*2/70, width*6/19, width/70], # 밑에서 두번째 ㅗ자
+						  [width*10/20, height*16/19-width*3/70, width/70, height*2/19+width/70], [width*7/20, height*18/19-width*3/70, width*6/19, width/70], # 맨 밑 ㅗ 자
+						  [0, height*14/19-width*2/70, width/19+width/70, width/70], # 맨 왼쪽 외벽쪽 -
+						  [width*19/20, height*14/19-width*2/70, width/19, width/70], # 맨 오른쪽 외벽쪽 -
+						  [width*3/20, height*14/19-width*2/70, width/70, height*2/19], [width*2/20, height*16/19-width*3/70, width/19, width/70], # L자
+						  [width*17/20, height*14/19-width*2/70, width/70, height*2/19], [width*17/20, height*16/19-width*3/70, width/19+width/70, width/70], # L자 좌우반전
+						  [width*2/20, height*18/19-width*3/70, width/19+width/70, width/70], # 맨 아래 왼쪽  -
+						  [width*17/20, height*18/19-width*3/70, width/19+width/70, width/70], # 맨 아래 오른쪽  -
+						  [width*5/20, height*16/19-width*3/70, width/70, height*4/19+width/70], [width*5/20, height*16/19-width*3/70, width*3/19, width/70], # ㅗ자 감싸는 wall 왼쪽
+						  [width*15/20, height*16/19-width*3/70, width/70, height*4/19+width/70], [width*12/20, height*16/19-width*3/70, width*3/19, width/70], # 맨 아래 ㅗ자 감싸는 박스 오른쪽
+						  ]
+
+		for wall_position in wall_positions:
+			wall = Wall(*wall_position, wall_color)
+			self.wall_sprites.add(wall)
+		return self.wall_sprites
+
+	# HARD version ghost zone 문 만들기
+	def setupGate(self, gate_color, width, height):
+		self.gate_sprites = pygame.sprite.Group()
+		self.gate_sprites.add(Wall(width*9/20+2, height*8/19-width/70, width*2/20, 2, gate_color))
+		return self.gate_sprites
+
+	# HARD version 캐릭터 구축
+	def setupPlayers(self, hero_image_path, ghost_images_path,width,height):
+		self.hero_sprites = pygame.sprite.Group()
+		self.ghost_sprites = pygame.sprite.Group()
+		self.hero_sprites.add(Player(287, 439, hero_image_path))
+		for each in ghost_images_path:
+			role_name = each.split('/')[-1].split('.')[0]
+			if role_name == 'Blinky':
+				player = Player(285, 259, each)
+				player.is_move = True
+				player.tracks = [[0, -0.5, 4], [0.5, 0, 9], [0, 0.5, 11], [0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 11], [0, 0.5, 3],
+								 [0.5, 0, 15], [0, -0.5, 15], [0.5, 0, 3], [0, -0.5, 11], [-0.5, 0, 3], [0, -0.5, 11], [-0.5, 0, 3],
+								 [0, -0.5, 3], [-0.5, 0, 7], [0, -0.5, 3], [0.5, 0, 15], [0, 0.5, 15], [-0.5, 0, 3], [0, 0.5, 3],
+								 [-0.5, 0, 3], [0, -0.5, 7], [-0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 11], [0, -0.5, 7], [0.5, 0, 5]]
+
+				self.ghost_sprites.add(player)
+			elif role_name == 'Clyde':
+				player = Player(319, 259, each)
+				player.is_move = True
+				player.tracks = [[-1, 0, 2], [0, -0.5, 4], [0.5, 0, 5], [0, 0.5, 7], [-0.5, 0, 11], [0, -0.5, 7],
+								 [-0.5, 0, 3], [0, 0.5, 7], [-0.5, 0, 7], [0, 0.5, 15], [0.5, 0, 15], [0, -0.5, 3],
+								 [-0.5, 0, 11], [0, -0.5, 7], [0.5, 0, 3], [0, -0.5, 11], [0.5, 0, 9]]
+				self.ghost_sprites.add(player)
+			elif role_name == 'Inky':
+				player = Player(255, 259, each)
+				player.is_move = True
+				player.tracks = [[1, 0, 2], [0, -0.5, 4], [0.5, 0, 10], [0, 0.5, 7], [0.5, 0, 3], [0, -0.5, 3],
+								 [0.5, 0, 3], [0, -0.5, 15], [-0.5, 0, 15], [0, 0.5, 3], [0.5, 0, 15], [0, 0.5, 11],
+								 [-0.5, 0, 3], [0, -0.5, 7], [-0.5, 0, 11], [0, 0.5, 3], [-0.5, 0, 11], [0, 0.5, 7],
+								 [-0.5, 0, 3], [0, -0.5, 3], [-0.5, 0, 3], [0, -0.5, 15], [0.5, 0, 15], [0, 0.5, 3],
+								 [-0.5, 0, 15], [0, 0.5, 11], [0.5, 0, 3], [0, -0.5, 11], [0.5, 0, 11], [0, 0.5, 3], [0.5, 0, 1]]
+				self.ghost_sprites.add(player)
+			elif role_name == 'Pinky':
+				player = Player(349, 259, each)
+				player.is_move = True
+				player.tracks = [[0, -1, 4], [0.5, 0, 9], [0, 0.5, 11], [-0.5, 0, 23], [0, 0.5, 7], [0.5, 0, 3],
+								 [0, -0.5, 3], [0.5, 0, 19], [0, 0.5, 3], [0.5, 0, 3], [0, 0.5, 3], [0.5, 0, 3],
+								 [0, -0.5, 15], [-0.5, 0, 7], [0, 0.5, 3], [-0.5, 0, 19], [0, -0.5, 11], [0.5, 0, 9]]
+				self.ghost_sprites.add(player)
+		return self.hero_sprites, self.ghost_sprites
+
+
+	# HARD version setupFood
+	def setupFood(self, food_color, bg_color, width, height):
+		self.food_sprites = pygame.sprite.Group()
+		# HARD : 가로 19개, 세로 19개 아이템 생성
+		# --> ROW = 19, COL = 19
+		for row in range(19):
+			for col in range(19):
+				if (row == 7 or row == 8) and (col == 7 or col == 8 or col == 9 or col == 10 or col == 11):
+					continue
+				else:
+					if ((row == 1 or row == 14) and (col == 0 or col == 18)) or ((row == 2 or row == 10) and (col == 0 or col == 3 or col == 10)):
+						food = Food(width/20*col+width/19, height/20*row+height/19, width/40, width/40, food_color, bg_color)
+					else:
+						food = Food(width/20*col+width/19, height/20*row+height/19, width/80, width/80, food_color, bg_color)
+				is_collide = pygame.sprite.spritecollide(food, self.wall_sprites, False)
+				if is_collide:
+					continue
+				is_collide = pygame.sprite.spritecollide(food, self.hero_sprites, False)
+				if is_collide:
+					continue
+				self.food_sprites.add(food)
+		return self.food_sprites
---
+++ Sprites.py
@@ -0,0 +1,134 @@
+'''
+Function:
+	定义一些精灵类
+Author:
+	Charles
+微信公众号:
+	Charles的皮卡丘
+'''
+import random
+import pygame
+import Fever
+
+
+'''墙类'''
+class Wall(pygame.sprite.Sprite):
+	def __init__(self, x, y, width, height, color, **kwargs):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width, height])
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+		self.rect.left = x
+		self.rect.top = y
+
+
+'''食物类'''
+class Food(pygame.sprite.Sprite):
+	def __init__(self, x, y, width, height, color, bg_color, **kwargs):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([width, height])
+		self.image.fill(bg_color)
+		self.image.set_colorkey(bg_color)
+		pygame.draw.ellipse(self.image, color, [0, 0, width, height])
+		self.rect = self.image.get_rect()
+		self.rect.left = x
+		self.rect.top = y
+		self.width=width
+		self.height=height
+
+'''角色类'''
+class Player(pygame.sprite.Sprite):
+
+	def __init__(self, x, y,role_image_path):
+		pygame.sprite.Sprite.__init__(self)
+		self.role_name = role_image_path.split('/')[-1].split('.')[0]
+		self.base_image = pygame.image.load(role_image_path).convert_alpha()
+		self.small_img = pygame.transform.scale(self.base_image,(31,31))
+		self.fever_img = pygame.image.load('resources/images/fever.png').convert()
+		self.g_base= pygame.image.load('resources/images/ghostfever.png').convert()
+		self.g_fever_img =pygame.transform.scale(self.g_base,(21,21))
+		self.image = self.small_img.copy()
+		self.rect = self.image.get_rect()
+		self.rect.left = x
+		self.rect.top = y
+		self.prev_x = x
+		self.prev_y = y
+		self.base_speed = [30, 30]
+		self.speed = [0, 0]
+		#self.speed_t=[0,0]
+		self.is_move = True
+		self.tracks = []
+		self.tracks_loc = [0, 0]
+		fever = Fever.Fever()
+	'''改变速度方向'''
+	def pchangeSpeed(self,isFeverTime,direction):
+		#self.change_x+=x
+		#self.change_y+=y
+		if isFeverTime:
+			if direction[0] < 0:
+				self.image = pygame.transform.flip(self.fever_img, True, False)
+			elif direction[0] > 0:
+				self.image = self.fever_img.copy()
+			elif direction[1] < 0:
+				self.image = pygame.transform.rotate(self.fever_img, 90)
+			elif direction[1] > 0:
+				self.image = pygame.transform.rotate(self.fever_img, -90)
+		else:
+			if direction[0] < 0:
+				self.image = pygame.transform.flip(self.small_img, True, False)
+			elif direction[0] > 0:
+				self.image = self.small_img.copy()
+			elif direction[1] < 0:
+				self.image = pygame.transform.rotate(self.small_img, 90)
+			elif direction[1] > 0:
+				self.image = pygame.transform.rotate(self.small_img, -90)
+		self.speed = [direction[0] * self.base_speed[0], direction[1] * self.base_speed[1]]
+		return self.speed
+
+	def gchangeSpeed(self,isFeverTime,hero_sprites,ghost_sprites,direction):
+		fever = Fever.Fever()
+		if isFeverTime==True and fever.feverTime(hero_sprites,ghost_sprites):
+			if direction[0] < 0:
+				self.image = pygame.transform.flip(self.g_fever_img, True, False)
+			elif direction[0] > 0:
+				self.image = self.g_fever_img.copy()
+			elif direction[1] < 0:
+				self.image = pygame.transform.rotate(self.g_fever_img, 90)
+			elif direction[1] > 0:
+				self.image = pygame.transform.rotate(self.g_fever_img, -90)
+		else:
+			if direction[0] < 0:
+				self.image = pygame.transform.flip(self.small_img, True, False)
+			elif direction[0] > 0:
+				self.image = self.small_img.copy()
+			elif direction[1] < 0:
+				self.image = pygame.transform.rotate(self.small_img, 90)
+			elif direction[1] > 0:
+				self.image = pygame.transform.rotate(self.small_img, -90)
+
+		self.speed = [direction[0] * self.base_speed[0], direction[1] * self.base_speed[1]]
+		return self.speed
+
+	def update(self, wall_sprites, gate_sprites):
+		if not self.is_move:
+			return False
+		x_prev = self.rect.left
+		y_prev = self.rect.top
+
+		self.rect.left += self.speed[0]
+		self.rect.top += self.speed[1]
+		is_collide = pygame.sprite.spritecollide(self, wall_sprites, False)
+		if is_collide:
+			self.rect.left = x_prev
+			self.rect.top = y_prev
+		if gate_sprites is not None:
+			if not is_collide:
+				is_collide = pygame.sprite.spritecollide(self, gate_sprites, True)
+		#if is_collide:
+			#self.rect.left = x_prev
+			#self.rect.top = y_prev
+			#return False
+		return True
+	'''生成随机的方向'''
+	def randomDirection(self):
+		return random.choice([[-0.5, 0], [0.5, 0], [0, 0.5], [0, -0.5]])
